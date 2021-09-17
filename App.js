49de6054c40c3baf2640ee29a7909f1413b8c954
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-09-17 18:47:15 -0500

    :art: [ADD] Tab layput

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Image, StyleSheet} from 'react-native';
 // * Components
 import CoinsStack from './src/components/coinsStack/CoinsStack';
+// * Styles
+import Colors from './src/res/colors';
+
+const Tabs = createBottomTabNavigator();
+const CoinsIconTab = require('./src/assets/bank.png');
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator style={styles.TabNavigator}>
+        <Tabs.Screen
+          name="Currencies"
+          options={{
+            tabBarIcon: ({size, color}) => (
+              <Image
+                style={{tintColor: color, width: size, height: size}}
+                source={CoinsIconTab}
+              />
+            ),
+          }}
+          component={CoinsStack}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  TabNavigator: {
+    backgroundColor: Colors.blackPearl,
+  },
+});
 
 export default App;
