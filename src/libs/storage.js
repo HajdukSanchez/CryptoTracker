9a2c02da403bb414a378be7e6dd4d0a98b58475a
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-09-20 06:32:18 -0500

    :art: [ADD] Storage class

---
+++ src/libs/storage.js
@@ -0,0 +1,52 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+class Storage {
+  static instance = Storage();
+
+  store = async (key, value) => {
+    try {
+      await AsyncStorage.setItem(key, value);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+
+  get = async key => {
+    try {
+      return await AsyncStorage.getItem(key);
+    } catch (error) {
+      console.log(error);
+      throw Error(error);
+    }
+  };
+
+  getAll = async keys => {
+    try {
+      return await AsyncStorage.multiGet(keys);
+    } catch (error) {
+      console.log(error);
+      throw Error(error);
+    }
+  };
+
+  getAllKeys = async () => {
+    try {
+      return await AsyncStorage.getAllKeys();
+    } catch (error) {
+      console.log(error);
+      throw Error(error);
+    }
+  };
+
+  remove = async key => {
+    try {
+      await AsyncStorage.removeItem(key);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+}
